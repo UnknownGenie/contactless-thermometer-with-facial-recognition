3a9dc3d3a39ed3c8a9720f4971de890d4d52ceb5
Uknown Genie <00faheem92@gmail.com>
2020-06-18 19:21:12 +0500

    Post request to server bug solved

--- send.py
+++ send.py
@@ -6,19 +6,22 @@ Created on Thu Jun 18 15:58:25 2020
 """
 
 import requests
-import time
 import argparse
+import time
+import sys
 
-def send_request(image, temperature, current_time, user_id): 
-    url = 'Localhost:5000/api/v1/device'
+def send_request(image, temperature, current_time, user_id, url): 
     request ={
-       "Image": image, 
-        "temperature": temperature,
+       "temperature": temperature,
          "time": current_time, 
         "User ID": user_id
     }
+    if isinstance(image, str):
+        files = {'media': open(image, 'rb')}
+    else:
+        files = {'media': image}
     try:
-        x = requests.post(url, data = request)
+        x = requests.post(url, data = request, files=files)
         return x
     except Exception as e:
         return e
@@ -31,10 +34,13 @@ if __name__ == '__main__':
     parser.add_argument("path", help="Path to image")
     
     args = parser.parse_args()
+    
+    url = 'Localhost:5000/api/v1/device'
+    
     temperature = "27.7C"
     user_id = "rada637367363" 
-    image = open(args.path, 'r+b')
+    image = args.path
     current_time = time.time()
     
-    response = send_request(image, temperature, current_time, user_id)
+    response = send_request(image, temperature, current_time, user_id, url)
     print(response)
