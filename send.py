c866f2f7f35b444d40e8619a88d52fed17834e05
Uknown Genie <00faheem92@gmail.com>
2020-06-18 21:15:55 +0500

    formatted server call

--- send.py
+++ send.py
@@ -6,41 +6,52 @@ Created on Thu Jun 18 15:58:25 2020
 """
 
 import requests
-import argparse
-import time
-import sys
 
 def send_request(image, temperature, current_time, user_id, url): 
     request ={
        "temperature": temperature,
-         "time": current_time, 
-        "User ID": user_id
+        "time": current_time, 
+        "userId": user_id
     }
-    if isinstance(image, str):
-        files = {'media': open(image, 'rb')}
-    else:
-        files = {'media': image}
+    
     try:
-        x = requests.post(url, data = request, files=files)
+        imencoded = cv2.imencode(".jpg", image)[1]
+        file = {'image': ('image.jpg', imencoded.tostring(), 'image/jpeg', {'Expires': '0'})}
+    except TypeError:
+        print("Please pass image as numpy array")
+        return 0
+    try:
+        x = requests.post(url, data = request, files=file)
         return x
     except Exception as e:
-        return e
+        print(e)
+        return 0
 
 if __name__ == '__main__':
-    
+    import cv2
+    import argparse
+    import time
+
     parser = argparse.ArgumentParser(description = ''' Send requests to server side.\n
                                      takes image path as input. set other variables itself''')
-                                 
     parser.add_argument("path", help="Path to image")
-    
     args = parser.parse_args()
     
-    url = 'Localhost:5000/api/v1/device'
-    
-    temperature = "27.7C"
+    # All variables
+    url = 'http://127.0.0.1:5000/api/v1/device'
+    temperature_float = 27.7
+    temperature_string = "{} C".format(temperature_float) # temperature foramtted as this 
     user_id = "rada637367363" 
-    image = args.path
-    current_time = time.time()
     
-    response = send_request(image, temperature, current_time, user_id, url)
+    # Get and format time
+    epoch_time = int(time.time())
+    formatted_time = time.ctime(epoch_time)
+    string_time = '{}'.format(formatted_time)
+    
+    # load image
+    image = cv2.imread(args.path)
+    
+    # call send_request
+    response = send_request(image, temperature_string, string_time, user_id, url)
     print(response)
+    
